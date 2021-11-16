3145c066c4265064b64669c8cc870aa47f3a4dd1
Mihalache Ștefan Cristian <57230349+StefanCsPurge@users.noreply.github.com>
2021-11-16 14:45:07 +0200

    added grammar

---
+++ Interpreter/grammar.py
@@ -0,0 +1,73 @@
+# 1c: LR(0)
+
+class Grammar:
+    def __init__(self, filename):
+        self.nonTerminals = []
+        self.terminals = []
+        self.starting = ''
+        self.productions = {}
+        self.filename = filename
+        self.getGrammarFromFile()
+
+    def getProductionString(self):
+        theStr = ''
+        for k in self.productions.keys():
+            theStr += k + ' -> '
+            for t in self.productions[k]:
+                theStr += t[0] + ' | '
+            theStr = theStr[:-3] + '\n'
+        return theStr
+
+    def getGrammarFromFile(self):
+        with open(self.filename, 'r') as file:
+            split_line = file.readline().strip().split('=')[1].split(' ')
+            for item in split_line:
+                self.nonTerminals.append(item)
+            split_line = file.readline().strip().split('=')[1].split(' ')
+            for item in split_line:
+                self.terminals.append(item)
+            self.starting = file.readline().strip().split('=')[1]
+            file.readline()
+            for line in file:
+                index = 1
+                lhs, rhs = line.split('->')
+                rhs = rhs.strip().split(' | ')
+                for value in rhs:
+                    if lhs in self.productions.keys():
+                        self.productions[lhs].append((value, index))
+                    else:
+                        self.productions[lhs] = [(value, index)]
+                    index += 1
+
+    def CFG_check(self):
+        for k in self.productions:
+            ks = k.split()
+            if len(ks) > 1:
+                return False
+            for elem in self.productions[k]:
+                alpha = elem[0].split()
+                for a in alpha:
+                    if a == 'E' or a in self.terminals or a in self.nonTerminals:
+                        continue
+                    else:
+                        return False
+        return True
+
+    def productions_for_nonTerminal(self, nonT):
+        print('Productions for non-terminal ' + str(nonT) + ': ')
+        theStr = ''
+        for k in self.productions.keys():
+            if k == nonT:
+                theStr += nonT + ' -> '
+                for t in self.productions[nonT]:
+                    theStr += t[0] + ' | '
+                theStr = theStr[:-3] + '\n'
+        print(theStr)
+
+    def __str__(self):
+        return 'non terminal = { ' + ', '.join(self.nonTerminals) + ' }\n' \
+               + 'terminal = { ' + ', '.join(self.terminals) + ' }\n' \
+               + 'starting = ' + str(self.starting) + '\n' \
+               + 'productions = { \n' + self.getProductionString() + '}\n'
+
+
--- Interpreter/main.py
+++ Interpreter/main.py
@@ -3,19 +3,27 @@
 
 from Scanner import Scanner
 from console import Console
+from grammar import Grammar
 
 if __name__ == '__main__':
     scanner = Scanner()
 
-    scanner.runScanner("p1.txt")
-
-    scanner.runScanner("p2.txt")
-
-    scanner.runScanner("p3.txt")
+    # scanner.runScanner("p1.txt")
+    #
+    # scanner.runScanner("p2.txt")
+    #
+    # scanner.runScanner("p3.txt")
 
     # scanner.runScanner("p1err.txt")
 
     # Finite automata console
-    # console = Console()
+    console = Console()
     # console.run()
+
+    gr = Grammar("g1.txt")
+    print(gr)
+    print("CFG check:",gr.CFG_check())
+    gr.productions_for_nonTerminal('C')
+
+
 
